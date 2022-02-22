9a859b2e4c7cedf52c095b2fdf282624f62a1a7f
Dmytro <shorop.d@gmail.com>
2022-02-22 10:11:26 +0200

    rules(), parseStyleRules(), createRegExp()

--- src/css_in_js.mjs
+++ src/css_in_js.mjs
@@ -56,7 +56,7 @@ export const combineNestedSelectors = (selectors) => {
 };
 
 // from here: https://github.com/ai/nanoid/blob/main/nanoid.js
-const nanoid = (t = 6) => {
+const nanoid = (t = 10) => {
   let e = "",
     r = crypto.getRandomValues(new Uint8Array(t));
   for (; t--; ) {
@@ -123,3 +123,261 @@ const _stylesheets = (styleSheets) => {
 _stylesheets.default = defaultStyleSheet;
 
 export const stylesheets = _stylesheets;
+
+/*****   PARSING STYLES AND RULES  *****/
+
+// from here: https://developer.mozilla.org/en-US/docs/Web/CSS/Reference   (on 19.02.2022)
+const validPseudoClasses = [
+  ":active",
+  "::after",
+  ":any-link",
+  "::backdrop",
+  "::before",
+  ":blank",
+  ":checked",
+  "::cue",
+  "::cue-region",
+  ":current",
+  ":default",
+  ":defined",
+  ":dir",
+  ":disabled",
+  ":empty",
+  ":enabled",
+  ":first",
+  ":first-child",
+  "::first-letter",
+  "::first-line",
+  ":first-of-type",
+  ":focus",
+  ":focus-visible",
+  ":focus-within",
+  ":fullscreen",
+  ":future",
+  "::grammar-error",
+  ":has",
+  ":host",
+  ":host-context",
+  ":hover",
+  ":in-range",
+  ":indeterminate",
+  ":invalid",
+  ":is",
+  ":lang",
+  ":last-child",
+  ":last-of-type",
+  ":left",
+  ":link",
+  ":local-link",
+  "::marker",
+  ":not",
+  ":nth-child",
+  ":nth-col",
+  ":nth-last-child",
+  ":nth-last-col",
+  ":nth-last-of-type",
+  ":nth-of-type",
+  ":only-child",
+  ":only-of-type",
+  ":optional",
+  ":out-of-range",
+  "::part",
+  ":past",
+  ":paused",
+  ":picture-in-picture",
+  "::placeholder",
+  ":placeholder-shown",
+  ":playing",
+  ":read-only",
+  ":read-write",
+  ":required",
+  ":right",
+  ":root",
+  ":scope",
+  "::selection",
+  "::slotted",
+  "::spelling-error",
+  ":target",
+  "::target-text",
+  ":target-within",
+  ":user-invalid",
+  ":user-valid",
+  ":valid",
+  ":visited",
+  ":where",
+];
+
+const isValidSelector = (selector) => {
+  if (!selector.includes(":") || /^\s*@/.test(selector)) return true;
+
+  for (const m of selector.match(/:[\w-:]*/g)) {
+    if (!validPseudoClasses.includes(m)) return false;
+  }
+  return true;
+};
+
+// padding: auto {...}
+//         /\
+//      space between ':' and value is obligatory!!
+const isValidPropertySegment = (segment) => /\w:(\s|$)/.test(segment);
+
+const isAtRule = (selector) => selector?.startsWith("@");
+
+const removeComments = (str) =>
+  str
+    .replaceAll(/\/\*.*?\*\//gs, "") // remove /* comment */
+    .replaceAll(/\/\/.*\n/g, ""); // remove // comment
+
+const removeConsecutiveWhitespaces = (text) =>
+  text.replaceAll(/\s+/g, " ").replaceAll(/ ,/g, ",");
+
+const parseProperty = (token) => {
+  const match = token.match(/(?<key>[\w-]+)\s*:\s*(?<value>[^:]*)/);
+  if (!match) return { key: token };
+
+  const { key, value } = match.groups;
+  return { key, value };
+};
+
+const removeEmptyRules = (rules) => {
+  Object.entries(rules).forEach(([selector, props]) => {
+    if (props instanceof Object && Object.keys(props).length === 0)
+      delete rules[selector];
+  });
+};
+
+const taggedTemplateArgsToString = (...args) => {
+  if (typeof args[0] === "string") return args[0];
+
+  const strings = args[0];
+  const segments = [strings[0]];
+  for (let i = 1; i < args.length; i++) {
+    segments.push(`${args[i]}${strings[i]}`);
+  }
+  return segments.join("");
+};
+
+export function parseStyleRules(...args) {
+  if (args.length === 0 || !args[0]) return {};
+
+  const text = removeConsecutiveWhitespaces(
+    removeComments(taggedTemplateArgsToString(...args))
+  );
+  const matches = [...text.matchAll(/(?<d>[{};]|$)/gs)];
+  let index = -1;
+  let depth = 0;
+  const blockTypes = [];
+  const parentTypes = [];
+  const selectorSegments = [];
+  const propKeySegments = [];
+  const tree = {};
+  const selectors = [tree];
+  const groupingRules = [tree];
+  let currentSelector;
+
+  const fullKey = (key) => {
+    if (propKeySegments.length === 0) return key;
+    return propKeySegments.join("-") + (key ? "-" + key : "");
+  };
+
+  for (let i = 0; i < matches.length; i++) {
+    const { d } = matches[i].groups;
+    const prevIndex = index;
+    index = matches[i].index;
+    const token = text.slice(prevIndex + 1, index).trim();
+
+    // console.log(i, token, groupingRules, selectors);
+    let type;
+    if (d === "{") {
+      if (isAtRule(token)) {
+        type = "@rule";
+        tree[token] = {};
+        groupingRules.unshift(tree[token]);
+      } else if (isValidSelector(token)) {
+        type = "selector";
+        selectorSegments.push(token);
+        currentSelector = combineNestedSelectors(selectorSegments);
+        groupingRules[0][currentSelector] = {};
+        selectors.unshift(groupingRules[0][currentSelector]);
+      } else if (isValidPropertySegment(token)) {
+        type = "prop-segment";
+        const { key, value } = parseProperty(token);
+        propKeySegments.push(key);
+
+        if (!!value) {
+          const parent =
+            parentTypes[0] === "@rule" ? groupingRules[0] : selectors[0];
+          parent[fullKey()] = value;
+        }
+      } else throw new SyntaxError(`Illegal selector '${token}'`);
+
+      blockTypes[++depth] = type;
+      if (type !== "prop-segment") parentTypes.unshift(type);
+
+      // d === '' means end of string
+    } else if (d === ";" || d === "") {
+      if (isAtRule(token)) {
+        tree[token] = null;
+      } else {
+        const { key, value } = parseProperty(token);
+
+        if (key && value && key != "" && value != "") {
+          const parent =
+            parentTypes[0] === "@rule" ? groupingRules[0] : selectors[0];
+          parent[fullKey(key)] = value;
+        }
+      }
+    } else if (d === "}") {
+      type = blockTypes[depth];
+      depth--;
+      if (type === "prop-segment") {
+        propKeySegments.pop();
+      } else if (type === "selector") {
+        selectorSegments.pop();
+        currentSelector = combineNestedSelectors(selectorSegments);
+        selectors.shift();
+      } else if (type === "@rule") {
+        groupingRules.shift();
+      }
+
+      if (parentTypes[0] === type) {
+        parentTypes.shift();
+      }
+
+      if (depth < 0)
+        throw new SyntaxError(
+          `Unmatched '}' at #${index}: "...${textRules.slice(
+            Math.max(index - 100, 0),
+            index + 1
+          )}" // <-`
+        );
+    }
+  }
+
+  if (depth > 0) throw new SyntaxError(`Unmatched '{'`);
+
+  removeEmptyRules(tree);
+  return tree;
+}
+
+/********** PARSING QUERY **********/
+
+/**
+ *  starts with / ??   remove / at start and end
+ *  spaces => \s+
+ *  .$\^:{} => \\
+ *  | split string and wrap in ()
+ *
+ *
+ * @param {*} textQuery
+ */
+export function createRegExp(textQuery) {
+  if (textQuery instanceof RegExp) return textQuery;
+  if (typeof textQuery !== "string" || textQuery?.length === 0) return null;
+
+  const source = textQuery
+    // .replaceAll(/^\s*\/|\/\s*$/g, "")
+    .replaceAll(/[+*()\[\].^$:{}\\]/g, "\\$&")
+    .replaceAll(/\s+/g, "\\s+");
+  return new RegExp(source);
+}
--- test/css_in_js.test.js
+++ test/css_in_js.test.js
@@ -4,7 +4,9 @@ import {
   combineNestedSelectors,
   createStyleSheet,
   deleteStyleSheet,
+  parseStyleRules,
   stylesheets,
+  createRegExp,
 } from "../src/css_in_js.mjs";
 
 const { expect } = chai;
@@ -85,7 +87,7 @@ describe(`deleteStyleSheet(styleSheet: CSSStyleSheet): boolean`, () => {
   });
 });
 
-describe(`stylesheets(styleSheets: CSSStyleSheet|CSSStyleSheet[]): CSSStyleSheet[]`, () => {
+describe(`stylesheets(styleSheets?: CSSStyleSheet|CSSStyleSheet[]): CSSStyleSheet[]`, () => {
   it(`should return Array of CSSStyleSheet's`, () => {
     expect(stylesheets()).to.be.instanceOf(Array);
   });
@@ -149,12 +151,453 @@ describe(`rules(filter?: string | string[] | RegExp | Query)`, () => {
     const ss = createStyleSheet();
     ss.insertRule(`.foo { color: red; }`, 0);
     ss.insertRule(`.bar { color: green; }`, 1);
-    ss.insertRule(`.baz { color: blue; }`, 2);
-    const css = stylesheets(ss);
+
+    const ss2 = createStyleSheet();
+    ss2.insertRule(`.baz { color: blue; }`);
+    const css = stylesheets([ss, ss2]);
     expect(css.rules()).to.have.lengthOf(3);
     expect(css.rules()[0].style).to.have.property("color", "red");
     // expect(stylesheets().rules())
 
     deleteStyleSheet(ss);
+    deleteStyleSheet(ss2);
   });
+});
+
+/*******  parseStyleRules  ********/
+describe(`parseStyleRules(textStyleRule?: string): RuleSet | StyleSet`, () => {
+  it(`can be used as function and tagged template (parseStyleRules\` .foo { color: red; }\`)`, () => {
+    expect(parseStyleRules(".foo { color: red; }")).to.be.deep.eql(
+      parseStyleRules`.foo { color: red; }`
+    );
+  });
+
+  it(`should return {} if 'textStyleRule' is not a string or empty string`, () => {
+    expect(parseStyleRules()).to.be.empty.and.to.be.an("object");
+    expect(parseStyleRules("")).to.be.empty.and.to.be.an("object");
+    expect(parseStyleRules(42)).to.be.empty.and.to.be.an("object");
+    expect(parseStyleRules({})).to.be.empty.and.to.be.an("object");
+    expect(parseStyleRules(``)).to.be.empty.and.to.be.an("object");
+  });
+
+  it(`property should always have string value (even for numbers)`, () => {
+    expect(parseStyleRules`
+      width: 0;
+      opacity: 0.5;   
+    `).to.be.eql({ width: "0", opacity: "0.5" });
+  });
+
+  it(`should ignore properties with empty values`, () => {
+    expect(parseStyleRules(`width: ; color: red;`)).to.be.eql({ color: "red" });
+    expect(
+      parseStyleRules(`
+      .foo {
+        width: ; 
+        color: red;
+      }`)
+    ).to.be.deep.eql({ ".foo": { color: "red" } });
+  });
+
+  it(`should ignore empty rules (without valid properties)`, () => {
+    expect(parseStyleRules`
+      body {}
+    `).to.be.empty;
+
+    expect(parseStyleRules`
+      body {
+        width: 10px
+      }
+    `).to.be.empty;
+  });
+
+  it(`should remove /* multi line comments */ and // one line comments`, () => {
+    expect(parseStyleRules`
+      body > div { // comment
+        opacity: /*
+           multi line
+           comment
+         */ 0.5;
+      }`).to.be.deep.eql({
+      "body > div": {
+        opacity: "0.5",
+      },
+    });
+  });
+
+  it(`multiple consecutive whitespaces, tabs, newlines should be replaced with single whitespace`, () => {
+    expect(parseStyleRules`
+        div     >    
+           img 
+             {
+               background-color   :   rgba(0  ,  0,
+                                                     0, 0.5);
+             }
+      `).to.be.deep.eql({
+      "div > img": {
+        "background-color": "rgba(0, 0, 0, 0.5)",
+      },
+    });
+  });
+  it(`properties should be delimited by ';', except when a property is the last token in the string and there is no } or { after it`, () => {
+    expect(parseStyleRules`
+          width: 10rem
+          color: red;
+        `).to.not.have.keys("color");
+
+    expect(parseStyleRules`
+          .foo {
+            width: 10rem;
+            color: red
+          }
+        `).to.be.deep.eql({
+      ".foo": {
+        width: "10rem",
+      },
+    });
+
+    expect(parseStyleRules`
+          width: 10rem;
+          color: red
+        `).to.be.deep.eql({ width: "10rem", color: "red" });
+  });
+
+  it(`should return StyleSet for text with properties only (without rule selector); properties must be delimited with ';'`, () => {
+    expect(parseStyleRules`
+        width: 10px;
+        color: red;
+      `).to.be.eql({ width: "10px", color: "red" });
+  });
+
+  it(`when a property is duplicated, the last occurence takes precedence: 'color: blue; color: red;' => 'color: red'`, () => {
+    expect(parseStyleRules`color: blue; color: red`).to.be.eql({
+      color: "red",
+    });
+  });
+
+  it(`should combine nested properties with '-': padding: 
+      {inline: { start: 1rem; end: 2rem; }} => padding-inline-start: 1rem; padding-inline-end; 2rem;`, () => {
+    expect(parseStyleRules`
+      padding: {
+        inline: {
+          start: 1rem;
+          end: 2rem;
+        }
+      }
+    `).to.be.deep.eql({
+      "padding-inline-start": "1rem",
+      "padding-inline-end": "2rem",
+    });
+  });
+
+  it(`a segment of nested property can have value; at least one whitespace between the value and preceding ':' is requied: 
+     margin: auto { right: 1rem; } => margin: auto; margin-right: 1rem;`, () => {
+    expect(parseStyleRules`
+        margin: auto {
+          right: 1rem;
+        }
+      `).to.be.deep.eql({
+      margin: "auto",
+      "margin-right": "1rem",
+    });
+  });
+
+  it(`should throw error, if there is no whitespace between ':' and property segment value`, () => {
+    expect(
+      () => parseStyleRules`
+      margin:auto {
+        right: 1rem;
+      }
+    `
+    ).to.throw();
+  });
+
+  it(`if nested property segment has value, but deeper segments - don't, then should return segment with value: maring: auto {}  =>  margin: auto`, () => {
+    expect(parseStyleRules`
+      margin: auto {}
+    `).to.be.eql({ margin: "auto" });
+  });
+
+  it(`if any of nested property segments has value, then ignore all of them`, () => {
+    expect(parseStyleRules`
+      margin: {
+        inline: {
+          
+        }
+      }
+    `).to.be.empty;
+  });
+
+  it(`selectors of single rule can be combined with ',': 'div > img, div > #id'`, () => {
+    expect(parseStyleRules` div > img, #id { color: red; }`).to.be.deep.eql({
+      "div > img, #id": {
+        color: "red",
+      },
+    });
+  });
+
+  it(`selectors can be spanned to multiple line; resulting selector is always one line`, () => {
+    expect(parseStyleRules`
+      body,
+      div { 
+        width: 10rem;
+      }
+    `).to.be.deep.eql({
+      "body, div": {
+        width: "10rem",
+      },
+    });
+  });
+
+  it(`should understand attribute-based selectors: [attr^="foo"]`, () => {
+    expect(parseStyleRules`
+      [attr^$|*="'foo'"] >+~ p {
+        color: red;
+      }
+    `).to.be.deep.eql({
+      "[attr^$|*=\"'foo'\"] >+~ p": {
+        color: "red",
+      },
+    });
+  });
+
+  it(`should understand valid pseudo-classes in selectors`, () => {
+    expect(parseStyleRules` p:hover { color: red; }`).to.be.deep.eql({
+      "p:hover": {
+        color: "red",
+      },
+    });
+
+    expect(parseStyleRules` ::before { color: red; }`).to.be.deep.eql({
+      "::before": {
+        color: "red",
+      },
+    });
+  });
+
+  it(`should throw error on invalid pseudo-classes`, () => {
+    expect(() => parseStyleRules`:foo { color: red; }`).to.throw();
+  });
+
+  it(`should combine nested selectors; nested selector can have placeholder '&', which will be replaced with content of parent selector`, () => {
+    expect(parseStyleRules`
+      div {
+        &__active {
+          color: red;
+        }
+      }
+    `).to.be.deep.eql({
+      div__active: {
+        color: "red",
+      },
+    });
+  });
+
+  it(`should combine nested selectors with single whitespace if there is no placeholder '&'`, () => {
+    expect(parseStyleRules`
+      div {
+        img {
+          color: red;
+        }
+      }
+    `).to.be.deep.eql({
+      "div img": {
+        color: "red",
+      },
+    });
+  });
+
+  it(`should create all possible unique combinations of nested selectors and combine them in one line, delimited with ','`, () => {
+    expect(parseStyleRules`
+      div, section {
+        .foo, & + #id {
+          &:hover {
+            color: red;
+          }
+        }
+      }
+    `).to.be.deep.eql({
+      "div .foo:hover, div + #id:hover, section .foo:hover, section + #id:hover":
+        {
+          color: "red",
+        },
+    });
+  });
+
+  it(`property can go after nested rule`, () => {
+    expect(parseStyleRules`
+      width: 1rem;
+      body {
+        img {
+          color: red;
+        }
+      }
+      opacity: 0;
+    `).to.be.deep.eql({
+      width: "1rem",
+      opacity: "0",
+      "body img": {
+        color: "red",
+      },
+    });
+  });
+
+  it(`@rule ending with ';' (like @import) should be stored on top of returned RuleSet in key; its value can be any`, () => {
+    expect(parseStyleRules`
+      @import url("style.css") screen;
+    `).to.have.all.keys('@import url("style.css") screen');
+  });
+
+  it(`@rule ending with '{' should be treated as a selector, which body can have properties or other selectors`, () => {
+    expect(parseStyleRules`
+      @page {
+        width: 2rem;
+        margin: {
+          right: 3rem;
+        }
+      }
+    `).to.be.deep.eql({
+      "@page": {
+        width: "2rem",
+        "margin-right": "3rem",
+      },
+    });
+
+    expect(parseStyleRules`
+      @keyframes slidein {
+        from {
+          transform: translateX(0%);
+        }
+
+        to {
+          transform: translateY(100%);
+        }
+      }
+    `).to.be.deep.eql({
+      "@keyframes slidein": {
+        from: {
+          transform: "translateX(0%)",
+        },
+        to: {
+          transform: "translateY(100%)",
+        },
+      },
+    });
+
+    expect(parseStyleRules`
+      @media (min-width: 500px) {
+        body {
+          color : blue;
+        }
+      }
+    `).to.be.deep.eql({
+      "@media (min-width: 500px)": {
+        body: {
+          color: "blue",
+        },
+      },
+    });
+  });
+
+  it(`should elevate @rule if it's nested, its inner rules should get properly combined with outer selectors`, () => {
+    expect(parseStyleRules`
+      body > div {
+        img {
+          width: 50%;
+        }
+        @media (min-width: 720px) {
+          img {
+            width: 20rem;
+          }
+        }
+      }
+    
+    `).to.be.deep.eql({
+      "body > div img": {
+        width: "50%",
+      },
+      "@media (min-width: 720px)": {
+        "body > div img": {
+          width: "20rem",
+        },
+      },
+    });
+  });
+
+  it(`should throw error on unbalanced braces {{ }`, () => {
+    expect(
+      () => parseStyleRules`
+      body {
+        {
+          width: 10rem;
+      }
+    `
+    ).to.throw();
+
+    expect(
+      () => parseStyleRules`
+      margin: {
+        right: 10rem;
+        {
+      }
+    `
+    ).to.throw();
+  });
+
+  it(`should throw error on unbalanced braces { }}`, () => {
+    expect(
+      () => parseStyleRules`
+      body {        
+          width: 10rem; }
+      }
+    `
+    ).to.throw();
+
+    expect(
+      () => parseStyleRules`
+      margin: {
+      }
+        right: 10rem;
+      
+      }
+    `
+    ).to.throw();
+  });
+});
+
+/**********  PARSING QUERY *********/
+describe(`createRegExp(textQuery: string): RegExp`, () => {
+  it(`should return RegExp for non-empty textQuery`, () => {
+    expect(createRegExp("foo")).to.be.instanceOf(RegExp);
+  });
+
+  it(`should return textQuery if it's RegExp`, () => {
+    const re = /foo/;
+    expect(createRegExp(re)).to.be.equal(re);
+  });
+
+  it(`should return null if textQuery is empty string or not a string or RegExp`, () => {
+    expect(createRegExp(42)).to.be.null;
+    expect(createRegExp("")).to.be.null;
+    expect(createRegExp([1, 2])).to.be.null;
+  });
+
+  // it(`'/' at start and/or end of textQuery should be ignored`, () => {
+  //   const source = createRegExp("/foo/").source;
+  //   expect(/^\s*\/|\/\s*$/g.test(source)).to.be.false;
+  // });
+
+  it(`should replace all whitespaces, tabs, newlines with \\s+`, () => {
+    const source = createRegExp(`foo  >   
+    div 
+    
+    `).source;
+    expect(source).to.be.equal("foo\\s+>\\s+div\\s+");
+  });
+
+  it(`+*()[].^$\:{}  should be escaped`, () => {
+    const source = createRegExp("^+*()[].\\:{}$").source;
+    expect(source).to.be.equal("\\^\\+\\*\\(\\)\\[\\]\\.\\\\\\:\\{\\}\\$");
+  });
+
+  // it(`should `)
 });
